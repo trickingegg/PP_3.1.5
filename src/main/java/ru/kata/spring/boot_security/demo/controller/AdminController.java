b6b4d9a858904b782eaa2b1c2757c17836ce4a6b
Andrew Bogatyryov <trickingegg@gmail.com>
2024-03-27 15:31:02 +0300

    Изменил контроллер админа и сделал стартовую страничку админа.

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -1,6 +1,7 @@
 package ru.kata.spring.boot_security.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.AuthenticatedPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -9,6 +10,7 @@ import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.RoleService;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
+import java.security.Principal;
 import java.util.List;
 
 @Controller
@@ -22,43 +24,55 @@ public class AdminController {
         this.userService = userService;
         this.roleService = roleService;
     }
-
     @GetMapping()
+    public String adminPage(Principal principal, Model model) {
+        String username = principal.getName();
+        User currentUser = userService.findByUsername(username);
+        if(currentUser != null) {
+            String fullName = currentUser.getName();
+            model.addAttribute("adminName", fullName);
+        } else {
+            model.addAttribute("adminName", "Undefined");
+        }
+        return "admin/admin";
+    }
+
+    @GetMapping("/userlist")
     public String getAllUsers(Model model) {
         List<User> users = userService.listUsers();
         model.addAttribute("userList", users);
-        return "admin/user-list";
+        return "admin/userList";
     }
 
     @DeleteMapping("/{id}")
     public String deleteUser(@PathVariable("id") Long id) {
         userService.delete(id);
-        return "redirect:/admin";
+        return "redirect:/admin/userlist";
     }
 
     @GetMapping("/create")
     public String getFormForCreateUser(Model model) {
         model.addAttribute("user", new User());
         model.addAttribute("roleList", roleService.allRoles());
-        return "admin/create-user"; // Аналогично изменен путь
+        return "admin/createUser";
     }
 
     @PostMapping("/create")
     public String createUser(@ModelAttribute("user") User user) {
         userService.add(user);
-        return "redirect:/admin";
+        return "redirect:/admin/userlist";
     }
 
     @GetMapping("/update/{id}")
     public String getFormForUpdateUser(@PathVariable("id") Long id, Model model) {
         model.addAttribute("user", userService.findById(id));
         model.addAttribute("roleList", roleService.allRoles());
-        return "admin/user-update";
+        return "admin/updateUser";
     }
 
     @PutMapping("/update")
     public String updateUser(@ModelAttribute("user") User user) {
         userService.update(user);
-        return "redirect:/admin";
+        return "redirect:/admin/userlist";
     }
 }
